93a124be7a57bd81fabf61d4dbd9dd8d6122e13a
彭军林 <boonya@sina.com>
2017-12-08 11:57:23 +0800

    添加项目文件。

---
+++ VedioShow/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace VedioShow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_Show = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.axPlayer1 = new AxAPlayer3Lib.AxPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.axPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Show
+            // 
+            this.btn_Show.Location = new System.Drawing.Point(28, 56);
+            this.btn_Show.Name = "btn_Show";
+            this.btn_Show.Size = new System.Drawing.Size(75, 23);
+            this.btn_Show.TabIndex = 1;
+            this.btn_Show.Text = "播放";
+            this.btn_Show.UseVisualStyleBackColor = true;
+            this.btn_Show.Click += new System.EventHandler(this.btn_Show_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Location = new System.Drawing.Point(133, 56);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(75, 23);
+            this.btn_stop.TabIndex = 4;
+            this.btn_stop.Text = "暂停";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 153);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "转码";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(133, 153);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "新播";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // axPlayer1
+            // 
+            this.axPlayer1.Enabled = true;
+            this.axPlayer1.Location = new System.Drawing.Point(250, 32);
+            this.axPlayer1.Name = "axPlayer1";
+            this.axPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axPlayer1.OcxState")));
+            this.axPlayer1.Size = new System.Drawing.Size(279, 166);
+            this.axPlayer1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 221);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.axPlayer1);
+            this.Controls.Add(this.btn_Show);
+            this.Name = "Form1";
+            this.Text = "VedioTest";
+            ((System.ComponentModel.ISupportInitialize)(this.axPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Show;
+        private AxAPlayer3Lib.AxPlayer axPlayer1;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ VedioShow/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+
+namespace VedioShow
+{
+    public partial class Form1 : Form
+    {
+        string path = AppDomain.CurrentDomain.BaseDirectory;
+        string pathVedio = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Show_Click(object sender, EventArgs e)
+        {
+            if (btn_Show.Text == "播放")
+            {
+                btn_Show.Text = "关闭";
+                pathVedio = path + "InitVideo1.mp4";
+                axPlayer1.Open(pathVedio);
+
+            }
+            else
+            {
+                btn_Show.Text = "播放";
+                axPlayer1.Close();
+            }
+        }
+
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            if (btn_stop.Text == "暂停")
+            {
+                btn_stop.Text = "开始";
+                axPlayer1.Pause();
+
+            }
+            else
+            {
+                btn_stop.Text = "暂停";
+                axPlayer1.Play();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Test1();
+        }
+
+        //原方法转码
+        private void Test1()
+        {
+
+            Process p = new Process();
+            
+
+            p.StartInfo.FileName = path +"ffmpeg.exe";
+
+            p.StartInfo.UseShellExecute = false;
+            string srcFileName = "";
+            string destFileName = "";
+            srcFileName = path + "InitVideo1.mp4";
+
+            destFileName = path + "InitVideo.mp4";
+
+            p.StartInfo.Arguments = "-i " + srcFileName + " -y  -vcodec h264 -b 500000 " + destFileName;    //执行参数
+
+            p.StartInfo.UseShellExecute = false;  ////不使用系统外壳程序启动进程
+            p.StartInfo.CreateNoWindow = true;  //不显示dos程序窗口
+
+            p.StartInfo.RedirectStandardInput = true;
+
+            p.StartInfo.RedirectStandardOutput = true;
+
+            p.StartInfo.RedirectStandardError = true;//把外部程序错误输出写到StandardError流中
+
+            p.ErrorDataReceived += new DataReceivedEventHandler(p_ErrorDataReceived);
+
+            p.OutputDataReceived += new DataReceivedEventHandler(p_OutputDataReceived);
+
+            p.StartInfo.UseShellExecute = false;
+
+            p.Start();
+
+            p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            p.BeginErrorReadLine();//开始异步读取
+
+
+
+            p.WaitForExit();//阻塞等待进程结束
+
+            p.Close();//关闭进程
+
+            p.Dispose();//释放资源
+        }
+        private static void p_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+
+            Console.WriteLine(e.Data);
+
+        }
+
+        private static void p_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+
+            Console.WriteLine(e.Data);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "新播")
+            {
+                button2.Text = "关闭";
+                axPlayer1.Open(path + "InitVideo.mp4");
+
+            }
+            else
+            {
+                button2.Text = "新播";
+                axPlayer1.Close();
+            }
+        }
+
+      
+    }
+}
